8e19d316701d19f7048d07653814a15f7db4f032
BɅVД <72298239+Fzthkm@users.noreply.github.com>
2024-11-26 22:58:56 +0300

    Initial Commit

---
+++ Storyboard and Interface Builder Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storyboard and Interface Builder Challenge
+//
+//  Created by Владислав Пасечник on 26.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
